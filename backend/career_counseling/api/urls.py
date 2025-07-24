775f644260e1b71b22020efbd883ebbf90db72bf
yuvraj051 <vyuvraj051@gmail.com>
2025-07-24 16:12:26 +0530

    Update URL patterns to include example endpoint in comments

--- backend/career_counseling/api/urls.py
+++ backend/career_counseling/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import StudentList
 
 urlpatterns = [
-    path('students/', StudentList.as_view()),
+    path('students/', StudentList.as_view()),  # http://127.0.0.1:8000/api/students/
 ]
