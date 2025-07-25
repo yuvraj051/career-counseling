c30aebcc07a22703b998d8b7c752e2664027e3f7
yuvraj051 <vyuvraj051@gmail.com>
2025-07-25 09:33:43 +0530

    Refactor student API views and update URL patterns for detail view

--- backend/career_counseling/api/urls.py
+++ backend/career_counseling/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import StudentList
+from .views import Student_view
 
 urlpatterns = [
-    path('students/', StudentList.as_view()),  # http://127.0.0.1:8000/api/students/
+    path('students/', Student_view.as_view()),  # http://127.0.0.1:8000/api/students/
+     path('students/<str:pk>/', Student_view.as_view(), name='student-detail'), # http://127.0.0.1:8000/api/students/<str:pk>/ for detail view
 ]
--- backend/career_counseling/api/views.py
+++ backend/career_counseling/api/views.py
@@ -4,9 +4,9 @@ from rest_framework import status
 from .models import Student
 from .serializers import StudentSerializer
 
-class StudentList(APIView):
+class Student_view(APIView):
     def get(self, request):
-        students = Student.objects.all()
+        students = Student.objects()
         print(students)
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
@@ -17,3 +17,22 @@ class StudentList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+            student.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def put(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+            serializer = StudentSerializer(student,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
