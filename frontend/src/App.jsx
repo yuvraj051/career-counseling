1a5a4bd9a116e1aa76a8367887d0a83396d16055
HardikMekhiya33 <hardikmekhiya33@gmail.com>
2025-07-28 16:15:34 +0530

    zyx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcomepage from './Components/Welcomepage';
 // import RegisterPage from './pages/RegisterPage'; (for later)
 // import LoginPage from './pages/LoginPage'; (for later)
-// sjkjkllj
+
 function App() {
   return (
     <Router>
