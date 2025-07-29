6feeaf64d66ec65d3b433098cbde8bbb1383ed7a
yuvraj051 <126749148+yuvraj051@users.noreply.github.com>
2025-07-30 00:30:44 +0530

    Delete frontend directory

--- frontend/src/Components/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/Dashboard.jsx
-import React from 'react';
-
-const Dashboard = () => {
-    return <h1 className="text-2xl text-center mt-20">🎉 Welcome to Your Dashboard!</h1>;
-};
-
-export default Dashboard;
