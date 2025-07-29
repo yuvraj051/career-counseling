bc940ce89382b1b9034c11c44a3ef324759d2e64
yuvraj051 <126749148+yuvraj051@users.noreply.github.com>
2025-07-30 00:30:59 +0530

    Delete backend directory

--- backend/career_counseling/api/models.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import Document, StringField, IntField
-
-class Student(Document):
-    # id= StringField(required=False)
-    name = StringField(required=True)
-    age = IntField(required=True)
